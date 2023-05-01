bf692c652339f6a5a5dbe86feb7e50211147064f
venfriti <tolusanusi6@gmail.com>
2023-05-01 20:22:25 +0100

    enabled permissions

--- app/src/main/java/com/example/MainActivity.kt
+++ app/src/main/java/com/example/MainActivity.kt
@@ -9,10 +9,21 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+
+
+
+enum class DownloadUrl(val value: Int) {
+    GLIDE(R.string.glide),
+    UDACITY(R.string.udacity),
+    RETROFIT(R.string.retrofit)
+}
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +34,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    private var downloadUrl: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -30,24 +43,59 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        val radioGroup: RadioGroup = findViewById(R.id.radio_group)
+        radioGroup.setOnCheckedChangeListener { _, checkedId ->
+            when (checkedId){
+                R.id.option_one -> {
+                    updateUrl(DownloadUrl.GLIDE)
+                    Toast.makeText(this, downloadUrl, Toast.LENGTH_SHORT).show()
+                }
+                R.id.option_two -> {
+                    updateUrl(DownloadUrl.UDACITY)
+                    Toast.makeText(this, downloadUrl, Toast.LENGTH_SHORT).show()
+                }
+                R.id.option_three -> {
+                    updateUrl(DownloadUrl.RETROFIT)
+                    Toast.makeText(this, downloadUrl, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
         custom_button.setOnClickListener {
             download()
+        }
+    }
+
+    private fun updateUrl(url: DownloadUrl){
+        downloadUrl = when (url){
+            DownloadUrl.GLIDE -> {
+                getString(R.string.glide_url)
+            }
+
+            DownloadUrl.UDACITY -> {
+                getString(R.string.udacity_url)
+            }
+
+            DownloadUrl.RETROFIT -> {
+                getString(R.string.retrofit_url)
+            }
         }
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+
         }
     }
 
 
-
     private fun download() {
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(downloadUrl))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
+                .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "filename.ext")
                 .setRequiresCharging(false)
                 .setAllowedOverMetered(true)
                 .setAllowedOverRoaming(true)
