4b78689cb6a6dec162b25e92660a84da24ea164c
venfriti <tolusanusi6@gmail.com>
2023-05-03 16:24:24 +0100

    add navigation to the button

--- app/src/main/java/com/example/DetailActivity.kt
+++ app/src/main/java/com/example/DetailActivity.kt
@@ -1,6 +1,8 @@
 package com.example
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.R
@@ -18,6 +20,12 @@ class DetailActivity : AppCompatActivity() {
             val notificationStatus = extras.getString("notification_status")
             val statusText = findViewById<TextView>(R.id.fileName)
             statusText.text = notificationStatus
+        }
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 
