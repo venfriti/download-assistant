50f21a369793eb022e2a0f1a5b8b11e9ba2ef94b
venfriti <tolusanusi6@gmail.com>
2023-07-12 13:36:09 +0100

    stable commit

--- app/src/main/java/com/example/MainFragment.kt
+++ app/src/main/java/com/example/MainFragment.kt
@@ -5,8 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 
 class MainFragment : Fragment() {
+
+//    private var downloadID: Long = 0
+//
+//    private lateinit var notificationManager: NotificationManager
+//
+//    private var downloadUrl: String = ""
+//    var fileName: String = ""
+//    private lateinit var addButton: FloatingActionButton
+//    private lateinit var urlEditText: EditText
+//    private lateinit var enterButton: Button
+//    private lateinit var optionFour: RadioButton
+//    private lateinit var urlLink: String
+//    private lateinit var userInput: String
+//    private lateinit var progressBar: ProgressBar
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
